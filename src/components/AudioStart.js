4d2ff978fe42cec7911112f9b76d3ba7eaee9cff
Curt Miller <curtdmiller@gmail.com>
2021-05-14 21:52:27 -0400

    Add audio start button overlay

--- src/components/AudioStart.js
+++ src/components/AudioStart.js
@@ -1,9 +1,37 @@
 import React from "react";
 import * as Tone from "tone";
 import { AppContext } from "../App";
+import { IconButton, makeStyles } from "@material-ui/core";
+import { PlayCircleOutline } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+  root: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    overflow: "hidden",
+    backgroundColor: "rgba(0,0,0,0.7)",
+    zIndex: 1000,
+    display: ({ isAudioStarted }) => (isAudioStarted ? "none" : "flex"),
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  startButton: {
+    color: "rgba(255,255,255,1)",
+    "&:hover": {
+      color: "rgba(255,255,255,0.9)"
+    },
+    "& .MuiSvgIcon-root": {
+      fontSize: "6em"
+    }
+  }
+});
 
 export default function AudioStartButton() {
   const { isAudioStarted, setIsAudioStarted } = React.useContext(AppContext);
+  const classes = useStyles({ isAudioStarted });
 
   async function handleAudioStart() {
     if (!isAudioStarted) {
@@ -12,5 +40,11 @@ export default function AudioStartButton() {
     }
   }
 
-  return <button onClick={handleAudioStart}>Start Audio</button>;
+  return (
+    <div className={classes.root}>
+      <IconButton onClick={handleAudioStart} className={classes.startButton}>
+        <PlayCircleOutline />
+      </IconButton>
+    </div>
+  );
 }
