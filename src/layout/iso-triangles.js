eef2bf28fcda115637ba3dfa29f527d71d2831f2
Curt Miller <curtdmiller@gmail.com>
2021-05-17 19:56:15 -0400

    Add more triangle patterns

--- src/layout/iso-triangles.js
+++ src/layout/iso-triangles.js
@@ -1,9 +1,18 @@
+import { makeStyles } from "@material-ui/core";
 import Fabric from "../components/Fabric";
 import { IsoTriangle } from "../components/shapes/Triangles";
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 200,
+    margin: "0 auto"
+  }
+});
+
 export default function IsoscelesTriangles() {
+  const classes = useStyles();
   return (
-    <>
+    <div className={classes.root}>
       <Fabric showGrid gridSize={[19, 19]} gridColor="black">
         <IsoTriangle orientation="north" size={13} fill="red" x={3} y={6} />
       </Fabric>
@@ -16,6 +25,18 @@ export default function IsoscelesTriangles() {
       <Fabric showGrid gridSize={[19, 19]} gridColor="black">
         <IsoTriangle orientation="west" size={13} fill="red" x={6} y={3} />
       </Fabric>
-    </>
+      <Fabric showGrid gridSize={[19, 19]} gridColor="black">
+        <IsoTriangle orientation="north" size={13} fill="red" x={3} y={6} />
+        <IsoTriangle orientation="south" size={13} fill="red" x={3} y={6} />
+        <IsoTriangle orientation="east" size={13} fill="red" x={6} y={3} />
+        <IsoTriangle orientation="west" size={13} fill="red" x={6} y={3} />
+      </Fabric>
+      <Fabric showGrid gridSize={[19, 19]} gridColor="black">
+        <IsoTriangle orientation="north" size={13} fill="red" x={3} y={12} />
+        <IsoTriangle orientation="south" size={13} fill="red" x={3} y={0} />
+        <IsoTriangle orientation="east" size={13} fill="red" x={0} y={3} />
+        <IsoTriangle orientation="west" size={13} fill="red" x={12} y={3} />
+      </Fabric>
+    </div>
   );
 }
