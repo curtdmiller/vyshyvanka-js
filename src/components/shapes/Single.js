478e92cd68f83f07864ba2de40d4ed5a19815ae8
Curt Miller <curtdmiller@gmail.com>
2021-05-25 21:55:50 -0400

    Match Single stitch state to ShapeBase stitches

--- src/components/shapes/Single.js
+++ src/components/shapes/Single.js
@@ -3,15 +3,22 @@ import G from "../G";
 import Stitch from "../Stitch";
 
 export default function Single({ fill, x, y, selected, setSelected }) {
+  const [stitchSelected, setStitchSelected] = React.useState(false);
   function clickHandler(event) {
-    if (event.shiftKey && setSelected && selected != null) {
+    if (!event.shiftKey && setSelected && selected != null) {
       setSelected(!selected);
     }
   }
 
   return (
     <G fillOpacity={selected ? 0.5 : 1} onClick={clickHandler}>
-      <Stitch fill={fill} x={x} y={y} />
+      <Stitch
+        fill={fill}
+        x={x}
+        y={y}
+        selected={stitchSelected}
+        handleClick={() => setStitchSelected(!stitchSelected)}
+      />
     </G>
   );
 }
