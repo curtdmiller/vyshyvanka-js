cf16613bce0df63dff8921274b2b4358bda9394e
Curt Miller <curtdmiller@gmail.com>
2021-06-02 21:55:45 -0400

    Update free draw mouse controls

--- src/layout/FreeDraw.js
+++ src/layout/FreeDraw.js
@@ -1,7 +1,8 @@
 import { Button, makeStyles, TextField } from "@material-ui/core";
 import * as React from "react";
+import { AppContext } from "../app-context";
 import Fabric from "../components/Fabric";
-import ShapeBase from "../components/shapes/ShapeBase";
+import G from "../components/G";
 
 const useStyles = makeStyles({
   controls: {
@@ -67,8 +68,66 @@ export default function FreeDraw() {
       </div>
 
       <Fabric gridSize={[size, size]} gridColor="#eaeaea" showGrid>
-        {stitches && <ShapeBase stitches={stitches} x={0} y={0} />}
+        {stitches && <ShapeBaseFreeDraw stitches={stitches} x={0} y={0} />}
       </Fabric>
     </div>
+  );
+}
+
+function ShapeBaseFreeDraw({ stitches, x, y, selected, setSelected }) {
+  const { cellSize, selectFill } = React.useContext(AppContext);
+  const [stitchState, setStitchState] = React.useState(
+    stitches.map((stitch) => ({
+      ...stitch,
+      selected: false,
+      originalFill: stitch.fill
+    }))
+  );
+
+  React.useEffect(() => {
+    setStitchState(
+      stitches.map((stitch) => ({
+        ...stitch,
+        selected: false,
+        originalFill: stitch.fill
+      }))
+    );
+  }, [stitches]);
+
+  function shapeClickHandler(event) {
+    if (event.shiftKey && setSelected && selected != null) {
+      setSelected(!selected);
+    }
+  }
+
+  function stitchClickHandler(event) {
+    if (!event.shiftKey) {
+      const newStitches = [...stitchState];
+      const cur = newStitches[event.currentTarget.dataset.id];
+      if (cur.fill != cur.originalFill && cur.fill != selectFill) {
+        cur.fill = selectFill;
+      } else {
+        cur.selected = !cur.selected;
+        cur.fill = cur.selected ? selectFill : cur.originalFill;
+      }
+      setStitchState(newStitches);
+    }
+  }
+
+  return (
+    <G x={x} y={y} fillOpacity={selected ? 0.5 : 1} onClick={shapeClickHandler}>
+      {stitchState.map((stitch, i) => (
+        <rect
+          width={cellSize}
+          height={cellSize}
+          fill={stitch.fill}
+          x={stitch.x * cellSize + 0.5}
+          y={stitch.y * cellSize + 0.5}
+          onClick={stitchClickHandler}
+          data-id={i}
+          key={`stitch-${i}`}
+        />
+      ))}
+    </G>
   );
 }
