3d945dc61a1b728151ec2d172672473c3345923f
Curt Miller <curtdmiller@gmail.com>
2021-06-02 21:44:18 -0400

    Add outer control labels

--- src/layout/star-diamond.js
+++ src/layout/star-diamond.js
@@ -7,16 +7,38 @@ import InnerTriangles from "./star-interface/InnerTriangles";
 import OuterDiamonds from "./star-interface/OuterDiamonds";
 import CornerStars from "./star-interface/CornerStars";
 import CenterStar from "./star-interface/CenterStar";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  labelWrapper: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  label: {
+    margin: 0
+  }
+});
 
 export default function StarDiamond() {
+  const classes = useStyles();
   return (
-    <Fabric gridSize={[37, 37]}>
-      <OuterTriangles />
-      <OuterDiamonds />
-      <InnerTriangles />
-      <CenterStar />
-      <Diamond diameter={25} cx={18} cy={18} stroke={colors.orange} />
-      <CornerStars />
-    </Fabric>
+    <>
+      <div className={classes.labelWrapper}>
+        <p className={classes.label}>Tempo -</p>
+        <p className={classes.label}>Tempo +</p>
+      </div>
+      <Fabric gridSize={[37, 37]}>
+        <OuterTriangles />
+        <OuterDiamonds />
+        <InnerTriangles />
+        <CenterStar />
+        <Diamond diameter={25} cx={18} cy={18} stroke={colors.orange} />
+        <CornerStars />
+      </Fabric>
+      <div className={classes.labelWrapper}>
+        <p className={classes.label}>Volume -</p>
+        <p className={classes.label}>Volume +</p>
+      </div>
+    </>
   );
 }
